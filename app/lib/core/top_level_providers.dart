067d1f1d46f9f5e276ec8e85c4990aabc2d38d31
bdeguigne <brice.deguigne@epitech.eu>
2023-10-06 01:58:18 +0400

    add supabase and getenv

--- app/lib/core/top_level_providers.dart
+++ app/lib/core/top_level_providers.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:runpasseo/presentation/routes/app_router.dart';
+import 'package:supabase_flutter/supabase_flutter.dart' as s;
 
 final Provider<AppRouter> routerProvider = Provider(
   (ProviderRef<AppRouter> ref) => AppRouter(),
@@ -9,3 +10,7 @@ final Provider<AppRouter> routerProvider = Provider(
 final Provider<GlobalKey<ScaffoldMessengerState>> scaffoldKeyProvider =
     Provider((ProviderRef<GlobalKey<ScaffoldMessengerState>> ref) =>
         GlobalKey<ScaffoldMessengerState>());
+
+final Provider<s.SupabaseClient> firebaseStorageProvider =
+    Provider<s.SupabaseClient>(
+        (ProviderRef<s.SupabaseClient> ref) => s.Supabase.instance.client);
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:runpasseo/core/top_level_providers.dart';
 import 'package:runpasseo/presentation/design_system/snackbars.dart';
 import 'package:runpasseo/presentation/design_system/theme.dart';
 import 'package:runpasseo/presentation/routes/app_router.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
+  await Supabase.initialize(
+    url: dotenv.get('SUPABASE_URL'),
+    anonKey: dotenv.get('SUPABASE_KEY'),
+  );
   runApp(
     const ProviderScope(
       child: MainApp(),
